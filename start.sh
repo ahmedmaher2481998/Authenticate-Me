6acb03d6d9e18ede7f6c67efcca71febbbd3d30a
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-10-30 21:34:16 +0200

    phase 1 -almost done2

--- start.sh
+++
@@ -1,4 +0,0 @@
-npm i cookie-parser cors csurf express dotenv express-async-errors helmet jsonwebtoken  morgan per-env sqlite3 sequelize sequelize-cli;
-npm i -D nodemon dotenv-cli;
-npx sequelize db:migrate;
-npm start;
