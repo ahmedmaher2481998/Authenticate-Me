f9d81599eae11bc63be108561e9023285cbaf464
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-10-30 20:06:09 +0200

    setup

---
+++ app.js
@@ -0,0 +1,9 @@
+console.clear();
+console.log(
+  "______________________________________________________________________________"
+);
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
