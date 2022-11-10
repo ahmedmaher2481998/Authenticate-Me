9d3c0ef0e6dfb25d58db55a0bff1e6267207faaa
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-11-10 18:54:58 +0200

    read to deploy

--- backend/utils/validationErrorHandler.js
+++ backend/utils/validationErrorHandler.js
@@ -2,7 +2,6 @@ const { validationResult } = require("express-validator");
 exports.validationErrorHandler = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors);
     const err = new Error();
     err.title = "Bad request.'";
     err.message = errors.errors.map((error) => `${error.msg}`);
